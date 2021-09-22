c0176cc0750b01e68a424c88198179fb72c9b4ff
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-09-22 22:16:07 +0900

    testの追加

--- user/tests/test_views.py
+++ user/tests/test_views.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+import unittest
+
 
 class HomeViewTests(TestCase):
     """HomeViewのテストクラス"""
@@ -55,14 +57,14 @@ class SuccessfulSignUpTests(TestCase):
         self.assertTrue(user.is_authenticated)
 
 
-class InvalidSignUpTests(TestCase):
+class NoneSignUpTests(TestCase):
     def setUp(self):
         """不適切なユーザを作成"""
         url = reverse('user:signup')
         self.response = self.client.post(url, {})
 
     def test_signup_status_code(self):
-        """ステータスコード200を返されることを確認"""
+        """無効なフォームを送信した場合、同じページに戻る"""
         self.assertEquals(self.response.status_code, 200)
 
     def test_form_errors(self):
@@ -75,26 +77,92 @@ class InvalidSignUpTests(TestCase):
         self.assertFalse(User.objects.exists())
 
 
-class LessPasswordSignUpTests(TestCase):
+class InvalidSignUpTests(TestCase):
     def setUp(self):
-        """短いパスワードのユーザを作成"""
+        """さまざまなエラーの出るユーザを作成"""
         url = reverse('user:signup')
-        data = {
+        """短いパスワード"""
+        less_password_data = {
             'username': 'example',
             'email': 'sample@example.com',
             'password1': '123',
             'password2': '123'
         }
-        self.response = self.client.post(url, data)
+        self.less_password_response = self.client.post(url, less_password_data)
+
+        """"@のないemail"""
+        invalid_email_data = {
+            'username': 'example',
+            'email': 'sample',
+            'password1': '12345678',
+            'password2': '12345678'
+        }
+        self.invalid_email_response = self.client.post(url, invalid_email_data)
+
+        """パスワードが一致していない"""
+        different_password_data = {
+            'username': 'example',
+            'email': 'sample@example.com',
+            'password1': '1234',
+            'password2': '12345678'
+        }
+        self.different_password_response = self.client.post(url, different_password_data)
+
+        """数字のみのパスワード"""
+        only_number_password_data = {
+            'username': 'example',
+            'email': 'sample@example.com',
+            'password1': '12345678',
+            'password2': '12345678'
+        }
+        self.only_number_password_response = self.client.post(url, only_number_password_data)
+
+        """ユーザ名に似たパスワード"""
+        like_username_and_password_data = {
+            'username': 'example',
+            'email': 'sample@example.com',
+            'password1': 'example13046',
+            'password2': 'example13046'
+        }
+        self.like_username_and_password_response = self.client.post(url, like_username_and_password_data)
+
+        """一般的すぎるパスワード"""
+        easy_password_data = {
+            'username': 'example',
+            'email': 'sample@example.com',
+            'password1': 'abcdefghi',
+            'password2': 'abcdefghi'
+        }
+        self.easy_password_data_response = self.client.post(url, easy_password_data)
 
     def test_signup_status_code(self):
         """ステータスコード200を返されることを確認"""
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEquals(self.less_password_response.status_code, 200)
+        self.assertEquals(self.invalid_email_response.status_code, 200)
+        self.assertEquals(self.different_password_response.status_code, 200)
+        self.assertEquals(self.only_number_password_response.status_code, 200)
+        self.assertEquals(self.like_username_and_password_response.status_code, 200)
+        self.assertEquals(self.easy_password_data_response.status_code, 200)
 
     def test_form_errors(self):
         """formエラーの検証"""
-        form = self.response.context.get('form')
-        self.assertTrue(form.errors)
+        less_password_form = self.less_password_response.context.get('form')
+        self.assertTrue(less_password_form.errors)
+
+        invalid_email_form = self.invalid_email_response.context.get('form')
+        self.assertTrue(invalid_email_form.errors)
+
+        different_password_form = self.different_password_response.context.get('form')
+        self.assertTrue(different_password_form.errors)
+
+        only_number_password_form = self.only_number_password_response.context.get('form')
+        self.assertTrue(only_number_password_form.errors)
+
+        like_username_and_password_form = self.like_username_and_password_response.context.get('form')
+        self.assertTrue(like_username_and_password_form.errors)
+
+        easy_password_data_form = self.easy_password_data_response.context.get('form')
+        self.assertTrue(easy_password_data_form.errors)
 
     def test_not_create_user(self):
         """ユーザが存在しないことを確認"""
