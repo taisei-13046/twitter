639d0af324eb41816e123baf6a682b246cc26ade
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-10-20 15:37:51 +0900

    formの共通化

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Post
 
 
-class PostCreateForm(forms.ModelForm):
+class PostFormBase(forms.ModelForm):
     class Meta:
         model = Post
         fields = (
@@ -15,14 +15,9 @@ class PostCreateForm(forms.ModelForm):
         }
 
 
-class PostUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = (
-            'content',
-        )
-        widgets = {
-            'content': forms.Textarea(
-                attrs={'rows': 2, 'cols': 70, 'placeholder': 'ここに入力'}
-            ),
-        }
+class PostCreateForm(PostFormBase):
+    pass
+
+
+class PostUpdateForm(PostFormBase):
+    pass
