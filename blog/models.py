fde27f1e6b00ebe22a1f0be873e1e194c9e25d21
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-12-22 15:22:24 +0900

    create model

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,12 @@ class Post(models.Model):
     class Meta:
         db_table = 'posts'
         ordering = ['-created_at']
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='like_user')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
