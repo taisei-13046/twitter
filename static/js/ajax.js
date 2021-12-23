2ffb66ea8f8dbcea243c48a3af1d172743f4ea6f
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-12-23 14:33:37 +0900

    大枠は完成、intenal server errorの解消が必要

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,10 +1,11 @@
 function getCookie(name) {
     var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
+    if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
             var cookie = jQuery.trim(cookies[i]);
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
@@ -15,40 +16,42 @@ function getCookie(name) {
 var csrftoken = getCookie('csrftoken');
 
 function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
-
 $.ajaxSetup({
-    crossDomain: false,
-    beforeSend: function (xhr, settings) {
-        if (!csrfSafeMethod(settings.type)) {
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
     }
 });
 
-$(document).ready(function(event){
-    $(document).on('click', '#like', function(event){
-        event.preventDefault();
-        var post_id = $(this).attr('name')
-        $.ajax({
-            type: 'POST',
-            url: ,
-            data: {
-                'post_id': $(this).attr('name'),
-                },
-            dataType: 'json',
-            success: function(response){
-                selector = document.getElementsByName(response.post_id);
-                if(response.liked){
-                    $(selector).html("<i class='fas fa-lg fa-heart'></i>");
-                }
-                else {
-                    $(selector).html("<i class='far fa-lg fa-heart'></i>");
-                }
-                selector2 = document.getElementsByName("count_" + response.post_id);
-                $(selector2).text(response.count);
+$(document).on('click', '#like', function(event){
+    event.preventDefault();
+    var url = $(this).attr('data-url')
+    var post_id = $(this).attr('name')
+    var split_url = $(this).attr('data-url').split('/');
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: {
+            'post_id': post_id,
+        },
+        dataType: 'json',
+        success: function(response){
+            if(response.liked){
+                var unlike = $(selector).attr('data-url').replace(split_url[2], 'unlike');
+                console.log(unlike)
+                $(selector).attr('data-url', unlike);
+                $(selector).html("<i class='fas fa-thumbs-up'></i>");
+            } else {
+                var like = $(selector).attr('data-url').replace(split_url[2], 'like');
+                $(selector).attr('data-url', like);
+                $(selector).html("<i class='far fa-thumbs-up'></i>");
             }
-        });
+            selector2 = document.getElementsByName("count_" + response.post_id);
+            $(selector2).text(response.count);
+        }
     });
 });
